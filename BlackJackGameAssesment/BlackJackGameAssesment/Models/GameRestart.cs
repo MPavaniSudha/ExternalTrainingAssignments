94029fd0f30f5c7a4f74641121d00b984ac279b9
MPavaniSudha <sudhavishnu7676@gmail.com>
2022-06-09 17:13:40 +0530

    BlackJackGame

---
+++ BlackJackGameAssesment/BlackJackGameAssesment/Models/GameRestart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlackJackGameAssesment.Models
+{
+    public class GameRestart
+    {
+        //It will restart the game
+        public void Restart()
+        {
+            Scores.Playerscore = 0;
+            Scores.Dealerscore = 0;
+            Scores.PlayerHitCount = 0;
+            Scores.DealerHitCount = 0;
+        }
+    }
+}
---
+++ BlackJackGameAssesment/BlackJackGameAssesment/Models/PlayingGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlackJackGameAssesment.Models
+{
+    public class PlayingGame
+    {
+        //It checks the  Scores of the Player&Dealer and display the result whether they win or lose
+        public string CheckGameStatus()
+        {
+            if (Scores.Playerscore > 21 && Scores.Dealerscore <= 21)
+            {
+                return "DealerWons,PlayerLoses";
+            }
+            else if (Scores.Dealerscore > 21 && Scores.Playerscore <= 21)
+            {
+                return "PlayerWons,DealerLoses";
+            }
+            else if (Scores.Playerscore == Scores.Dealerscore)
+            {
+                return "GameTie";
+            }
+            else if (Scores.Playerscore <= 21)
+            {
+                if (Scores.Playerscore < Scores.Dealerscore)
+                {
+                    return "PlayerWons, DealerLoses";
+                }
+                else
+                {
+                    return "DealerWons,PlayerLoses";
+                }
+            }
+            return "OOps!!Something Went Wrong";
+        }
+
+    }
+}
